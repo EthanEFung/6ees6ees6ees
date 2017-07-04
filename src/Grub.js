e6800bee321eca73a5fc6d6b420fdf49cd821c7b
Ethan Fung <EthanEFung@gmail.com>
2017-07-04 08:35:43 -0700

    Write class declaration Grub.js in ES6
    Things to note:
    the 'constructor' keyword. It seems that
    within the constructor, the desired
    properties are appended to the object
    we are instantiating.
    
    Also methods are declared within the the
    scope of the class declaration.  It seems
    to imply that as long as the the method
    is not declared within the constructor,
    the method is appended to the prototype.

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,16 @@
 class Grub {
+  //Research the constructor keyword, it seems to be implying
+  //that whatever is placed within, will be become the properties
+  //of the object the class keyword instantiates.
+  constructor() {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+  
+  //Research to see if this is the proper syntax of declaring
+  //methods that are placed upon the __proto__.
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
 };

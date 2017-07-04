0b618525874618b7243ddf98b4bd353558c0d9b9
Ethan Fung <EthanEFung@gmail.com>
2017-07-04 08:46:30 -0700

    Write class declaration subclass Bee.js
    Things to remember:
    (1) The super() method is the equivalent
    to SuperClass.call(this).  And this must
    be called before properties or methods
    are declared on the subclass.
    
    (2)extends key word is the equivalent to
    Subclass.prototype = Object.create(Superclass.
    prototype); Seems to be that extends will
    point the __proto__ to an instance of the
    another objects prototype that is specified.

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
-class Bee {
-  // TODO..
+//Declare Bee as the subclass of Grub
+class Bee extends Grub {
+  //have a constructor method
+  constructor() {
+  	//Write the equivalent to 'Grub.call(this)'
+  	super();
+  	this.age = 5;
+  	this.color = 'yellow';
+  	this.job = 'Keep on growing';
+  }
+
+  //write other methods here.
 };
